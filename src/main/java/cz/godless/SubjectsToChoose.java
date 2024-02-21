21bf0ea828ca3d845e581552fa675bdb5c6e8281
1GodLess5 <gacekalbert@seznam.cz>
2024-02-21 07:08:33 +0100

    new Enum SubjectsToChoose

---
+++ src/main/java/cz/godless/SubjectsToChoose.java
@@ -0,0 +1,32 @@
+package cz.godless;
+
+public enum SubjectsToChoose {
+    MATHEMATICS("Mathematics"),
+    MUSIC("Music"),
+    SOCIAL_STUDIES("Social studies"),
+    DRAMA("Drama"),
+    COMPUTING("Computing"),
+    LITERATURE("Literature"),
+    SCIENCE("Science"),
+    ART("Art"),
+    GEOGRAPHY("Georgraphy"),
+    IT("IT"),
+    GRAPHIC_DESIGN("Graphic design"),
+    PHILOSOPHY("Philosophy"),
+    P_E("P/E"),
+    HISTORY("History"),
+    LANGUAGES("Languages"),
+    BIOLOGY("Biology"),
+    CHEMISTRY("Chemistry"),
+    ENGLISH("English");
+
+    private final String description;
+
+    SubjectsToChoose(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
