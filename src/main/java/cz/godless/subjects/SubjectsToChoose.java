cdfe21b63863a461a92a060c4e8b9457f0abf381
1GodLess5 <gacekalbert@seznam.cz>
2024-02-26 08:34:25 +0100

    changed subjectCount to start from 1

--- src/main/java/cz/godless/subjects/SubjectsToChoose.java
+++ src/main/java/cz/godless/subjects/SubjectsToChoose.java
@@ -1,24 +1,24 @@
 package cz.godless.subjects;
 
 public enum SubjectsToChoose {
-    MATHEMATICS("Mathematics", 0),
-    MUSIC("Music", 1),
-    SOCIAL_STUDIES("Social studies", 2),
-    DRAMA("Drama", 3),
-    COMPUTING("Computing", 4),
-    LITERATURE("Literature", 5),
-    SCIENCE("Science", 6),
-    ART("Art", 7),
-    GEOGRAPHY("Geography", 8),
-    IT("IT", 9),
-    GRAPHIC_DESIGN("Graphic design", 10),
-    PHILOSOPHY("Philosophy", 11),
-    P_E("P/E", 12),
-    HISTORY("History", 13),
-    LANGUAGES("Languages", 14),
-    BIOLOGY("Biology", 15),
-    CHEMISTRY("Chemistry", 16),
-    ENGLISH("English", 17);
+    MATHEMATICS("Mathematics", 1),
+    MUSIC("Music", 2),
+    SOCIAL_STUDIES("Social studies", 3),
+    DRAMA("Drama", 4),
+    COMPUTING("Computing", 5),
+    LITERATURE("Literature", 6),
+    SCIENCE("Science", 7),
+    ART("Art", 8),
+    GEOGRAPHY("Geography", 9),
+    IT("IT", 10),
+    GRAPHIC_DESIGN("Graphic design", 11),
+    PHILOSOPHY("Philosophy", 12),
+    P_E("P/E", 13),
+    HISTORY("History", 14),
+    LANGUAGES("Languages", 15),
+    BIOLOGY("Biology", 16),
+    CHEMISTRY("Chemistry", 17),
+    ENGLISH("English", 18);
 
     private final String description;
     private final int subjectCount;
