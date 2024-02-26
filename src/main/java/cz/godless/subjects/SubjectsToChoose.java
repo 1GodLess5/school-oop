47636a1baae5dce08202da4fe644269e282f0e1e
1GodLess5 <gacekalbert@seznam.cz>
2024-02-26 07:43:07 +0100

    added subjectCount

--- src/main/java/cz/godless/subjects/SubjectsToChoose.java
+++ src/main/java/cz/godless/subjects/SubjectsToChoose.java
@@ -1,32 +1,38 @@
-package cz.godless;
+package cz.godless.subjects;
 
 public enum SubjectsToChoose {
-    MATHEMATICS("Mathematics"),
-    MUSIC("Music"),
-    SOCIAL_STUDIES("Social studies"),
-    DRAMA("Drama"),
-    COMPUTING("Computing"),
-    LITERATURE("Literature"),
-    SCIENCE("Science"),
-    ART("Art"),
-    GEOGRAPHY("Geography"),
-    IT("IT"),
-    GRAPHIC_DESIGN("Graphic design"),
-    PHILOSOPHY("Philosophy"),
-    P_E("P/E"),
-    HISTORY("History"),
-    LANGUAGES("Languages"),
-    BIOLOGY("Biology"),
-    CHEMISTRY("Chemistry"),
-    ENGLISH("English");
+    MATHEMATICS("Mathematics", 0),
+    MUSIC("Music", 1),
+    SOCIAL_STUDIES("Social studies", 2),
+    DRAMA("Drama", 3),
+    COMPUTING("Computing", 4),
+    LITERATURE("Literature", 5),
+    SCIENCE("Science", 6),
+    ART("Art", 7),
+    GEOGRAPHY("Geography", 8),
+    IT("IT", 9),
+    GRAPHIC_DESIGN("Graphic design", 10),
+    PHILOSOPHY("Philosophy", 11),
+    P_E("P/E", 12),
+    HISTORY("History", 13),
+    LANGUAGES("Languages", 14),
+    BIOLOGY("Biology", 15),
+    CHEMISTRY("Chemistry", 16),
+    ENGLISH("English", 17);
 
     private final String description;
+    private final int subjectCount;
 
-    SubjectsToChoose(String description){
+    SubjectsToChoose(String description, int subjectCount){
         this.description = description;
+        this.subjectCount = subjectCount;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public int getSubjectCount() {
+        return subjectCount;
     }
 }
