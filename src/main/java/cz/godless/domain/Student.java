54360c05eb95d67a1a82d2d4a61ba300ad5f2e12
1GodLess5 <gacekalbert@seznam.cz>
2024-02-28 08:26:50 +0100

    reformat changes

--- src/main/java/cz/godless/domain/Student.java
+++ src/main/java/cz/godless/domain/Student.java
@@ -1,23 +1,14 @@
 package cz.godless.domain;
 
-import cz.godless.subjects.Subject;
-
-import java.util.List;
-
 public class Student {
     private final String name;
-    private final List<Subject> subjects;
 
-    public Student(String name, List<Subject> subjects){
+    public Student(String name){
         this.name = name;
-        this.subjects = subjects;
     }
 
     public String getName() {
         return name;
     }
 
-    public List<Subject> getSubjects() {
-        return subjects;
-    }
 }
