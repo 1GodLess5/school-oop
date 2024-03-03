c734be879ccca5db3b52453a01b2ba329449e423
1GodLess5 <gacekalbert@seznam.cz>
2024-03-03 11:09:47 +0100

    testing of Subject

--- src/main/java/cz/godless/service/MainManager.java
+++ src/main/java/cz/godless/service/MainManager.java
@@ -2,44 +2,27 @@ package cz.godless.service;
 
 import cz.godless.domain.Student;
 import cz.godless.subjects.Subject;
-import cz.godless.utility.InputUtils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainManager {
-    private final List<Student> studentList;
+    private final List<Subject> subjects;
 
     public MainManager() {
-        this.studentList = new ArrayList<>();
+        this.subjects = new ArrayList<>();
     }
 
 
     public void school(){
-        System.out.println("Enter student name: ");
-        String studentName = InputUtils.readString();
+        Subject mathematics = new Subject("Mathematics");
+        mathematics.addGrades("Albert");
 
-        Map<String, List<Float>> studentSubjects = new HashMap<>();
-        int userInput = 0;
-        do {
-            System.out.println("0 - Exit");
-            Subject.printSubjects();
-            System.out.println("Which subject do you want to add?");
-            userInput = InputUtils.readInt();
+        this.subjects.add(mathematics);
 
-            if (userInput != 0){
-                String chosenSubject = Subject.chooseSubject(userInput);
-                if (chosenSubject == null){
-                    System.out.println("Invalid input.");
-                } else {
-                    // TODO GRADES ADD
-                }
-
-            }
-        } while (userInput != 0);
-
+        SubjectManager subjectManager = new SubjectManager(this.subjects);
+        subjectManager.addStudentToSubject();
+        subjectManager.printSubjects();
 
     }
 }
